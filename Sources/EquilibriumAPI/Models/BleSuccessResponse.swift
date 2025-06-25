eb73d21ea68c94c8f442ec790a1af63a9f953be7
Leo Wehrfritz <leocommander.cs@gmail.com>
2025-06-25 17:05:02 +0200

    Implemented a lot of base functionality

---
+++ Sources/EquilibriumAPI/Models/BleSuccessResponse.swift
@@ -0,0 +1,10 @@
+//
+//  BleAdvertisementResponse.swift
+//  EquilibriumAPI
+//
+//  Created by Leo Wehrfritz on 24.06.25.
+//
+
+public struct BleSuccessResponse: Codable {
+    public let success: Bool
+}
